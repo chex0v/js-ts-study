5c9b5935c8c81266cadc95cc5835ac949e996f44
chexov <chex0ff@inbox.ru>
2021-01-06 21:30:55 +0300

    Added first test in ts

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      targets: {
+        browsers: '> 3%'
+        // browsers: '> 3%, ie 11' // ie 11 transpiles classes
+      }
+    }],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties'
+  ]
+};
